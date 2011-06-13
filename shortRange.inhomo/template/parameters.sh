0e4e6059da80c8c2c4b2bb743fef520ab77936fa
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2011-06-13 05:16:34 +0000

    add template

---
+++ shortRange.inhomo/template/parameters.sh
@@ -0,0 +1,16 @@
+device=1
+
+confFile=confs/N00010000-T0.7-100x020x020.gro
+dt=0.005
+nstep=2000000
+confFeq=2000
+thermoFeq=100
+rcut=10.0
+nlistExten=0.3
+
+refT=0.7
+tauT=1.0
+
+NTCell=96
+NTAtom=96
+
---
+++ shortRange.inhomo/template/run.uniform.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source parameters.sh
+
+function set_parameter () {
+    file=$1
+    sed -e "/IndexType nstep/s/=.*/= $nstep;/g" $file |\
+    sed -e "/IndexType confFeq/s/=.*/= $confFeq;/g" |\
+    sed -e "/IndexType thermoFeq/s/=.*/= $thermoFeq;/g"|\
+    sed -e "/ScalorType rcut/s/=.*/= $rcut;/g" |\
+    sed -e "/ScalorType nlistExten/s/=.*/= $nlistExten;/g" |\
+    sed -e "/ScalorType refT/s/=.*/= $refT;/g" |\
+    sed -e "/ScalorType tauT/s/=.*/= $tauT;/g" |\
+    sed -e "/ScalorType dt/s/=.*/= $dt;/g" |\
+    sed -e "/^\#define NThreadsPerBlockCell/s/[0-9][0-9]*/$NTCell/g" |\
+    sed -e "/^\#define NThreadsPerBlockAtom/s/[0-9][0-9]*/$NTAtom/g" > tmp
+    mv -f tmp $file
+}
+
+
+set_parameter tools/simulation.uniform.cutoff/app/lj.nhc.cu
+make -j4 -C tools/simulation.uniform.cutoff/ &> make.log
+rm -f conf.gro
+cp $confFile conf.gro
+workdir=`pwd`
+command="$workdir/tools/simulation.uniform.cutoff/lj.nhc conf.gro $nstep $device"
+echo command is: $command
+
+
+
