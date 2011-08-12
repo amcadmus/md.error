d62251d00407eb65928c00a8fda66dbb3a570db3
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2011-08-12 08:53:11 +0000

    fix a bug rc>10

--- shortRange.inhomo/template/tools/sh.scripts/tension.uniform.sh
+++ shortRange.inhomo/template/tools/sh.scripts/tension.uniform.sh
@@ -22,7 +22,7 @@ tension_error=`echo "$tension_error * 2" | bc -l`
 
 rcut=`grep "rcut=" parameters.sh | cut -d '=' -f 2`
 make -C tools/analyze.simulation.suit -j 4 &> /dev/null
-tension_corr=`tools/analyze.simulation.suit/analyze.uniform -s $st_start -r $rcut | grep tension | awk '{print $3}'`
+tension_corr=`tools/analyze.simulation.suit/analyze.uniform -s $st_start -r $rcut --rcmax $rcut | grep tension | awk '{print $3}'`
 tension_corr_print=`printf $print_format $tension_corr`
 
 tension_total=`echo "$tension_corr + $tension" | octave | grep ans | awk '{print $3}'`
