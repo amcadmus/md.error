dfcf3967c4583315faa5c7fcb6179c963544b7f0
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2011-10-04 17:18:52 +0000

    prec is 5

--- shortRange.inhomo/template/tools/sh.scripts/rho.sh
+++ shortRange.inhomo/template/tools/sh.scripts/rho.sh
@@ -4,10 +4,11 @@ l1=70
 l2=80
 g1=20
 g2=130
+prec=5
 
 cd tools/
 svn up
 cd ..
 make -C tools/cal.density.profile -j4
 
-tools/cal.density.profile/density --check-point-liquid-1 $l1 --check-point-liquid-2 $l2 --check-point-gas-1 $g1 --check-point-gas-2 $g2 -s 5000
+tools/cal.density.profile/density -p $prec --check-point-liquid-1 $l1 --check-point-liquid-2 $l2 --check-point-gas-1 $g1 --check-point-gas-2 $g2 -s 5000
