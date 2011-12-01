ce2ea68bebbf26fd0c9f4731767a32f05d87d538
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2011-12-01 16:41:11 +0000

    add support for uniform distribution

--- longRange.hetero/profile.test/template/parameters.sh
+++ longRange.hetero/profile.test/template/parameters.sh
@@ -19,10 +19,13 @@ peak_dist=5.0
 project_name=two.peaks
 profile_command="$project_name -d $peak_dist -x $Lx -y $Ly -z $Lz -p $peak_size -t $layerWith -u $rhoh -l $rhol -n 0"
 
+project_name=uniform
+profile_command="$project_name -x $Lx -y $Ly -z $Lz --rho $rhoh -n 0"
+
 # Ewald parameters
-beta=0.6
+beta=1.0
 ref_rcut=5.0
-ref_K=31
+ref_K=65
 # regulate parameters. DO NOT MOVE
 beta=`printf "%.3f" $beta`
 ref_rcut=`printf "%.2f" $ref_rcut`
@@ -31,7 +34,7 @@ record_dir=$project_name.b$beta.r$ref_rcut.K$ref_K
 
 # naive summation
 cal_rcut=3.0
-cal_K=21
+cal_K=59
 cal_order=6
 
 real_h=1.0
