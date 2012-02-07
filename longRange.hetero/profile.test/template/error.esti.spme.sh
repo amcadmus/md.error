3701ace061b0c960c4b084f0faec42f9ce104a73
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2012-02-07 12:29:13 +0000

    cp gp files to result dir

--- longRange.hetero/profile.test/template/error.esti.spme.sh
+++ longRange.hetero/profile.test/template/error.esti.spme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source parameters.sh
+
 if test ! -d $record_dir; then
     echo "no record dir $record_dir"
     exit
@@ -13,3 +15,9 @@ fi
 ./tools/scripts/spme.ik.esti.sh
 ./tools/scripts/spme.ana.real.sh
 ./tools/scripts/spme.ana.esti.sh
+
+gp_file_names="pl.ana.error.gp  pl.ana.meanf.gp  pl.ik.error.gp  pl.ik.meanf.gp"
+for gp_file_name in $gp_file_names; do
+#    echo "test ! -f $errors_dir/$gp_file_name && cp tools/gp.scripts/$gp_file_name $errors_dir"
+    test ! -f $errors_dir/$gp_file_name && cp tools/gp.scripts/$gp_file_name $errors_dir
+done
