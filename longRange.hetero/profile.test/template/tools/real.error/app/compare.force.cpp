78d3d03a231d352c92b718343c605e547dfa2187
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2012-02-13 15:00:52 +0000

    fix bug: calculate only positive real error

--- longRange.hetero/profile.test/template/tools/real.error/app/compare.force.cpp
+++ longRange.hetero/profile.test/template/tools/real.error/app/compare.force.cpp
@@ -20,12 +20,13 @@ namespace po = boost::program_options;
 int main(int argc, char * argv[])
 {
   po::options_description desc ("Allow options");
-  std::string config, force0, force1, out, meanf;
+  std::string config, force0, force1, out, meanf, qfile;
   double refh;
   
   desc.add_options()
       ("help,h", "print this message")
       ("config,c", po::value<std::string > (&config)->default_value("conf.gro"), "config file")
+      ("charge-table,q", po::value<std::string > (&qfile), "the charge table")
       ("refh", po::value<double > (&refh)->default_value(1.), "bin size")
       ("force-0", po::value<std::string > (&force0)->default_value("force0.out"), "force file 0")
       ("force-1", po::value<std::string > (&force1)->default_value("force1.out"), "force file 1")
@@ -64,7 +65,7 @@ int main(int argc, char * argv[])
   
   int nbin = tmpbox[0] / refh + 0.5;
   double hx = tmpbox[0] / double(nbin);
-  std::vector<int > count(nbin, 0);
+  // std::vector<int > count(nbin, 0);
   std::vector<int > countp(nbin, 0);
 
   std::vector<double > dir_value  (nbin, 0.);
@@ -82,6 +83,27 @@ int main(int argc, char * argv[])
   std::vector<double > t_meanfy (nbin, 0.);
   std::vector<double > t_meanfz (nbin, 0.);
 
+  std::vector<double > charge (posi.size(), 1.);
+  for (unsigned i = posi.size() / 2; i < posi.size(); ++i){
+    charge[i] = -1.;
+  }
+  if (vm.count("charge-table")){
+    FILE * fptable = fopen(qfile.c_str(), "r");
+    if (fptable == NULL){
+      std::cerr << "cannot open file " << qfile << std::endl;
+      exit (1);
+    }
+    for (unsigned i = 0; i < posi.size(); ++i){
+      double tmpvalue;
+      int returnvalue = fscanf(fptable, "%lf", &tmpvalue);
+      if (returnvalue != 1){
+	std::cerr << "wrong format of file " << qfile << std::endl;
+	exit(1);
+      }
+      charge[i] = tmpvalue;
+    }
+    fclose (fptable);
+  }
   
   for (unsigned i = 0; i < posi.size(); ++i){
     if (posi[i][0] >= tmpbox[0]) posi[i][0] -= tmpbox[0];
@@ -112,7 +134,7 @@ int main(int argc, char * argv[])
     t_f0x -= t_f1x;
     t_f0y -= t_f1y;
     t_f0z -= t_f1z;
-    if (i < posi.size() / 2){
+    if (charge[i] > 0){
       dir_meanfx[idx] += dir_f0x;
       dir_meanfy[idx] += dir_f0y;
       dir_meanfz[idx] += dir_f0z;
@@ -122,6 +144,9 @@ int main(int argc, char * argv[])
       t_meanfx[idx] += t_f0x;
       t_meanfy[idx] += t_f0y;
       t_meanfz[idx] += t_f0z;
+      dir_value[idx] += dir_f0x*dir_f0x + dir_f0y*dir_f0y + dir_f0z*dir_f0z;
+      rec_value[idx] += rec_f0x*rec_f0x + rec_f0y*rec_f0y + rec_f0z*rec_f0z;
+      t_value[idx] += t_f0x*t_f0x + t_f0y*t_f0y + t_f0z*t_f0z;
       countp[idx] ++;
     }
     // else {
@@ -130,10 +155,7 @@ int main(int argc, char * argv[])
     //   meanfz[idx] -= f0z;
     //   countp[idx] ++;
     // }
-    dir_value[idx] += dir_f0x*dir_f0x + dir_f0y*dir_f0y + dir_f0z*dir_f0z;
-    rec_value[idx] += rec_f0x*rec_f0x + rec_f0y*rec_f0y + rec_f0z*rec_f0z;
-    t_value[idx] += t_f0x*t_f0x + t_f0y*t_f0y + t_f0z*t_f0z;
-    count[idx] += 1;
+    // count[idx] += 1;
   }
   for (int i = 0; i < nbin; ++i){
     if (countp[i] != 0){
@@ -146,12 +168,12 @@ int main(int argc, char * argv[])
       t_meanfx[i] /= countp[i];
       t_meanfy[i] /= countp[i];
       t_meanfz[i] /= countp[i];
+      dir_value[i] = sqrt (dir_value[i] / countp[i]);
+      rec_value[i] = sqrt (rec_value[i] / countp[i]);
+      t_value[i] = sqrt (t_value[i] / countp[i]);
     }
-    if (count[i] != 0){
-      dir_value[i] = sqrt (dir_value[i] / count[i]);
-      rec_value[i] = sqrt (rec_value[i] / count[i]);
-      t_value[i] = sqrt (t_value[i] / count[i]);
-    }
+    // if (count[i] != 0){
+    // }
   }
   
   fclose (fp0);
@@ -170,7 +192,7 @@ int main(int argc, char * argv[])
 	    dir_value[i],
 	    rec_value[i],
 	    t_value[i],
-	    count[i]
+	    countp[i]
 	);
   }
   fclose (fp);
