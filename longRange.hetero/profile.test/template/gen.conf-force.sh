54ed0c1ed0d8843683d2abf5ef09cc99e7befa75
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2012-02-14 22:04:40 +0000

    fix bug in gen.conf-force.sh

--- longRange.hetero/profile.test/template/gen.conf-force.sh
+++ longRange.hetero/profile.test/template/gen.conf-force.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+source parameters.sh
+
 if echo $project_prefix | grep water &> /dev/null; then
+    if test -d $record_dir; then
+	echo "# find $record_dir, backup"
+	mv $record_dir $record_dir.`date +%s`
+    fi
     cp -a $water_template $record_dir
     echo "# gen conf from traj"
     ./tools/scripts/gen.from.traj.sh
