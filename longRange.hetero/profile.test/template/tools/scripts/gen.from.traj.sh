b43e7feb9b86569450541dad2ff1d5d311b4fb3c
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2012-02-14 16:00:21 +0000

    add gen.from.traj.sh

---
+++ longRange.hetero/profile.test/template/tools/scripts/gen.from.traj.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source parameters.sh
+
+mylog=gen.from.traj.log
+
+rm -f $mylog
+if test ! -f $record_dir/traj.xtc; then
+    echo "# no file $record_dir/traj.xtc"
+    exit
+fi
+if test ! -f $record_dir/charge.tab; then
+    echo "# no file $record_dir/charge.tab"
+    exit
+fi
+cp parameters.sh $record_dir/parameters.gen.from.traj.sh
+
+make -j8 -C ./tools/profile/ &> make.log
+
+./tools/profile/split.traj -i $record_dir/traj.xtc &>> $mylog
+mv -f conf*gro $record_dir/
+
+cd $record_dir/
+rm -f tmp
+mkdir tmp
+mv `ls | grep conf | grep gro | head -n $nframe_real_error ` tmp
+rm -f conf*gro
+mv tmp/*gro .
+rm -f tmp
+cd -
+
+mv -f make.log $mylog $record_dir
+
+
