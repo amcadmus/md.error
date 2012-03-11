e6124956214fbb273850b22b5d61cda7790a80d3
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2012-03-11 14:22:54 +0000

    revise sh scripts

--- longRange.hetero/profile.test/template/error.esti.spme.sh
+++ longRange.hetero/profile.test/template/error.esti.spme.sh
@@ -7,19 +7,42 @@ if test ! -d $record_dir; then
     exit
 fi
 
-if test ! -f $errors_dir/esti.dir.error.out; then
-    echo "# esti spme dir"
-    ./tools/scripts/dir.esti.sh
-fi  
-
-echo "# real spme ik"
-./tools/scripts/spme.ik.real.sh
+echo "# esti spme dir"
+./tools/scripts/dir.esti.sh
 echo "# esti spme ik"
 ./tools/scripts/spme.ik.esti.sh
-echo "# real spme ana"
-./tools/scripts/spme.ana.real.sh
 echo "# esti spme ana"
 ./tools/scripts/spme.ana.esti.sh
+echo "# esti spme ana self"
+./tools/scripts/spme.ana.selfCorr.esti.sh
+echo "# esti spme st"
+./tools/scripts/spme.st.esti.sh
+
+if echo $project_name | grep -v rand | grep water; then
+    echo "# real spme ik noHO"
+    ./tools/scripts/spme.ik.noHO.real.sh
+    echo "# real spme ik noHO st"
+    ./tools/scripts/spme.ik.noHO.st.real.sh
+else
+    echo "# real spme ik"
+    ./tools/scripts/spme.ik.real.sh
+    echo "# real spme ik st"
+    ./tools/scripts/spme.ik.st.real.sh
+fi
+
+if echo $project_name | grep -v rand | grep water; then
+    echo "# real spme ana noHO"
+    ./tools/scripts/spme.ana.noHO.real.sh
+    echo "# real spme ana noHO st"
+    ./tools/scripts/spme.ana.noHO.st.real.sh
+else
+    echo "# real spme ana"
+    ./tools/scripts/spme.ana.real.sh
+    echo "# real spme ana selfCorr"
+    ./tools/scripts/spme.ana.selfCorr.real.sh
+    echo "# real spme ana st"
+    ./tools/scripts/spme.ana.st.real.sh
+fi
 
 gp_file_names="pl.ana.error.gp  pl.ana.meanf.gp  pl.ik.error.gp  pl.ik.meanf.gp"
 for gp_file_name in $gp_file_names; do
--- longRange.hetero/profile.test/template/gen.conf-force.sh
+++ longRange.hetero/profile.test/template/gen.conf-force.sh
@@ -15,12 +15,18 @@ if echo $project_name | grep water | grep -v rand &> /dev/null; then
     cp -a $water_template $record_dir
     echo "# gen conf from traj"
     ./tools/scripts/gen.from.traj.sh
+    echo "# make ref"
+#    ./tools/scripts/make.ref.sh
+    ./tools/scripts/make.noHO.ref.sh
 else
     echo "# gen conf"
     ./tools/scripts/gen.conf.sh
+    echo "# make ref"
+    ./tools/scripts/make.ref.sh
 fi
 
-echo "# make ref"
-./tools/scripts/make.ref.sh
+#echo "# make ref"
+#./tools/scripts/make.ref.sh
+#./tools/scripts/make.noHO.ref.sh
 
 
