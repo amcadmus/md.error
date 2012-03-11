a474e34fe403ba79e7d7dce227c5127d4649576e
wanghan <wanghan@a28d8dd4-3dfb-4ec0-b98a-e5cf011a4b41>
2012-03-11 14:45:54 +0000

    water do all simulations

--- longRange.hetero/profile.test/template/error.esti.spme.sh
+++ longRange.hetero/profile.test/template/error.esti.spme.sh
@@ -18,11 +18,16 @@ echo "# esti spme ana self"
 echo "# esti spme st"
 ./tools/scripts/spme.st.esti.sh
 
-if echo $project_name | grep -v rand | grep water; then
+if echo $project_name | grep -v rand | grep water &> /dev/null; then
     echo "# real spme ik noHO"
     ./tools/scripts/spme.ik.noHO.real.sh
     echo "# real spme ik noHO st"
     ./tools/scripts/spme.ik.noHO.st.real.sh
+
+    echo "# real spme ik"
+    ./tools/scripts/spme.ik.real.sh
+    echo "# real spme ik st"
+    ./tools/scripts/spme.ik.st.real.sh
 else
     echo "# real spme ik"
     ./tools/scripts/spme.ik.real.sh
@@ -30,11 +35,18 @@ else
     ./tools/scripts/spme.ik.st.real.sh
 fi
 
-if echo $project_name | grep -v rand | grep water; then
+if echo $project_name | grep -v rand | grep water &> /dev/null; then
     echo "# real spme ana noHO"
     ./tools/scripts/spme.ana.noHO.real.sh
     echo "# real spme ana noHO st"
     ./tools/scripts/spme.ana.noHO.st.real.sh
+
+    echo "# real spme ana"
+    ./tools/scripts/spme.ana.real.sh
+    echo "# real spme ana selfCorr"
+    ./tools/scripts/spme.ana.selfCorr.real.sh
+    echo "# real spme ana st"
+    ./tools/scripts/spme.ana.st.real.sh
 else
     echo "# real spme ana"
     ./tools/scripts/spme.ana.real.sh
@@ -44,7 +56,7 @@ else
     ./tools/scripts/spme.ana.st.real.sh
 fi
 
-gp_file_names="pl.ana.error.gp  pl.ana.meanf.gp  pl.ik.error.gp  pl.ik.meanf.gp"
+gp_file_names="pl.water.ana.error.gp pl.water.ik.error.gp pl.ana.error.gp  pl.ana.meanf.gp  pl.ik.error.gp  pl.ik.meanf.gp"
 for gp_file_name in $gp_file_names; do
 #    echo "test ! -f $errors_dir/$gp_file_name && cp tools/gp.scripts/$gp_file_name $errors_dir"
     test ! -f $errors_dir/$gp_file_name && cp tools/gp.scripts/$gp_file_name $errors_dir
