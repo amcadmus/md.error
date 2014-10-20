92c674314589dfcb452a9fb7e084ba6d0da79ad7
amcadmus <amcadmus@gmail.com>
2014-10-20 17:49:39 +0200

    split out the lj and ele force scalors

--- force.kernel/test.kernels/include/nb_interactions.h
+++ force.kernel/test.kernels/include/nb_interactions.h
@@ -32,34 +32,66 @@ struct nb_interaction_compute_fe_tag		{} ;
 struct nb_interaction_compute_fev_tag		{} ;
 struct nb_interaction_compute_fv_tag		{} ;
 
-
+template <int MDDIM, typename ValueType>
+inline void
+nb_pair_force_lj_scale (const ValueType *		vdwParam,
+			const ValueType &		dist2,
+			ValueType &			fscale,
+			nb_interaction_accelteration_none_tag,
+			nb_interaction_vanderWaals_null_tag)
+{
+}
 
 template <int MDDIM, typename ValueType>
 inline void
-nb_pair_force_impl_nocut (const ValueType *		eleParam,
-			  const ValueType *		vdwParam,
-			  const ValueType *		dof0,
-			  const ValueType *		dof1,
-			  const ValueType *		charge0,
-			  const ValueType *		charge1,
-			  const ValueType &		dist2,
-			  const ValueType *		diff,
-			  ValueType *			energy,
-			  ValueType *			force,
-			  ValueType *			fshift,
-			  nb_interaction_geometric_none_tag,
-			  nb_interaction_accelteration_none_tag,
-			  nb_interaction_electrostatic_null_tag,
-			  nb_interaction_vanderWaals_cutoff_tag,
-			  nb_interaction_compute_f_tag)
+nb_pair_force_lj_scale (const ValueType *		vdwParam,
+			const ValueType &		dist2,
+			ValueType &			fscale,
+			nb_interaction_accelteration_none_tag,
+			nb_interaction_vanderWaals_cutoff_tag)
 {
   ValueType ri2 = 1./dist2;
   ValueType sri2 = vdwParam[LennardJones6_12::sigma] * vdwParam[LennardJones6_12::sigma] * ri2;
   ValueType sri6 = sri2*sri2*sri2;
-  ValueType scalor = 24 * vdwParam[LennardJones6_12::epsilon] * (ValueType(2) * (sri6*sri6) - sri6) * ri2;
-
-  nb_auxiliary_scalor_multiply <MDDIM, ValueType> (scalor, diff, force);
+  fscale += 24 * vdwParam[LennardJones6_12::epsilon] * (ValueType(2) * (sri6*sri6) - sri6) * ri2;
 }
+
+template <int MDDIM, typename ValueType>
+inline void
+nb_pair_force_ele_scale (const ValueType *		eleParam,
+			 const ValueType &		charge0,
+			 const ValueType &		charge1,
+			 const ValueType &		dist2,
+			 ValueType &			fscale,
+			 nb_interaction_accelteration_none_tag,
+			 nb_interaction_electrostatic_null_tag)
+{
+}
+
+// template <int MDDIM, typename ValueType>
+// inline void
+// nb_pair_force_impl_nocut (const ValueType *		eleParam,
+// 			  const ValueType *		vdwParam,
+// 			  const ValueType *		dof0,
+// 			  const ValueType *		dof1,
+// 			  const ValueType *		charge0,
+// 			  const ValueType *		charge1,
+// 			  const ValueType &		dist2,
+// 			  const ValueType *		diff,
+// 			  ValueType &			energy,
+// 			  ValueType &			fscale,
+// 			  ValueType *			fshift,
+// 			  nb_interaction_geometric_none_tag,
+// 			  nb_interaction_accelteration_none_tag,
+// 			  nb_interaction_electrostatic_null_tag,
+// 			  nb_interaction_vanderWaals_cutoff_tag,
+// 			  nb_interaction_compute_f_tag)
+// {
+//   ValueType ri2 = 1./dist2;
+//   ValueType sri2 = vdwParam[LennardJones6_12::sigma] * vdwParam[LennardJones6_12::sigma] * ri2;
+//   ValueType sri6 = sri2*sri2*sri2;
+//   fscale = 24 * vdwParam[LennardJones6_12::epsilon] * (ValueType(2) * (sri6*sri6) - sri6) * ri2;
+// }
 
 
 // template <typename ValueType,
@@ -107,13 +139,20 @@ nb_pair_force_impl (const ValueType *		eleParam,
   ValueType dist2 = nb_auxiliary_dist2<MDDIM, ValueType>  (diff);
   if (dist2 > cutoff[0] * cutoff[0]) return;
 
-  nb_pair_force_impl_nocut <MDDIM, ValueType>
-      (eleParam, vdwParam, dof0, dof1, charge0, charge1, dist2, diff, energy, force, fshift,
-       nb_interaction_geometric_none_tag (),
-       Acceleration (),
-       ElectrostaticType (),
-       VanderWaalsType (),
-       ComputeMode() );
+  ValueType fscale (0);
+  // nb_pair_force_impl_nocut <MDDIM, ValueType>
+  //     (eleParam, vdwParam, dof0, dof1, charge0, charge1, dist2, diff, *energy, fscale, fshift,
+  //      nb_interaction_geometric_none_tag (),
+  //      Acceleration (),
+  //      ElectrostaticType (),
+  //      VanderWaalsType (),
+  //      ComputeMode() );
+  nb_pair_force_ele_scale <MDDIM, ValueType>
+      (eleParam, charge0[0], charge1[0], dist2, fscale, Acceleration(), ElectrostaticType());  
+  nb_pair_force_lj_scale <MDDIM, ValueType>
+      (vdwParam, dist2, fscale, Acceleration(), VanderWaalsType());
+  
+  nb_auxiliary_scalor_multiply <MDDIM, ValueType> (fscale, diff, force);
 }
 				
 
