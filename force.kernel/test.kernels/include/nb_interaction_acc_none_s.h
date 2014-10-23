4bc251b4b92508315594ab97524d25247f916c2f
amcadmus <amcadmus@gmail.com>
2014-10-23 16:51:46 +0200

    add missing files

---
+++ force.kernel/test.kernels/include/nb_interaction_acc_none_s.h
@@ -0,0 +1,17 @@
+#ifndef __nb_interaction_acc_none_s_h_wanghan__
+#define __nb_interaction_acc_none_s_h_wanghan__
+
+#include "nb_interaction_operators.h"
+
+struct nb_interaction_accelteration_none_s_tag	{
+public:
+  typedef float		ValueType;
+  typedef float		DataType;
+  typedef int		IndexType;
+  static const int	index_stride = 1;
+}
+    ;
+
+#endif
+
+
---
+++ force.kernel/test.kernels/include/nb_interaction_compute.h
@@ -0,0 +1,7 @@
+#ifndef __nb_interaction_compute_h_wanghan__
+#define __nb_interaction_compute_h_wanghan__
+
+struct nb_interaction_compute_energy_no_tag	{} ;
+struct nb_interaction_compute_energy_yes_tag	{} ;
+
+#endif
